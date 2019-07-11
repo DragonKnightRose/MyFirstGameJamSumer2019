c1978dce64f3f65a3ad92db006435e7136a75e52
Gwendolynn Eskridge <gwendolynn.eskridge@gmail.com>
2019-07-10 21:31:43 -0500

    Added GUI and player signals to communicate with it, created new spider enemy and web projectile

--- Characters/Player.gd
+++ Characters/Player.gd
@@ -3,10 +3,11 @@ extends KinematicBody2D
 const CHARACTER_NAME = "Leah"
 export (int) var SPEED = 100
 
-signal damage
+signal damaged
 signal heal
 signal death
 signal set_health
+signal set_max_health
 
 var velocity = Vector2()
 var MAX_HEALTH = 3
@@ -16,6 +17,7 @@ var defense = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	health = MAX_HEALTH
+	emit_signal("set_max_health", health)
 	emit_signal("set_health", health)
 
 
@@ -24,6 +26,15 @@ func _physics_process(delta):
 	get_movement()
 	velocity = move_and_collide(velocity*delta)
 
+func hit(damage):
+	print("Player hit for {0} damage!".format([str(damage)]))
+	health -= damage
+	if health <= 0:
+		print("player died!")
+		emit_signal("death")
+	else:
+		print("{0} health remaining".format([str(health)]))
+		emit_signal("set_health",health)
 
 func get_movement():
 	var movement = Vector2()
